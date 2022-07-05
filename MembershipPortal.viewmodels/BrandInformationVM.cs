d45e5df205d406e2e0d28632cf1c872cb91c46c8
Samuel Esezobor <prodigy4801@gmail.com>
2022-07-05 10:04:10 +0100

    Domain model Commit

---
+++ MembershipPortal.viewmodels/BrandInformationVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipPortal.viewmodels
+{
+    public class BrandInformationVM
+    {
+        public int id { get; set; }
+        public string brandname { get; set; }
+        public string registrationid { get; set; }
+        public DateTime datecreated { get; set; }
+        public DateTime datemodified { get; set; }
+    }
+
+    public class BrandInformationVM_CRU
+    {
+        public int id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string brandname { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string registrationid { get; set; }
+    }
+}
