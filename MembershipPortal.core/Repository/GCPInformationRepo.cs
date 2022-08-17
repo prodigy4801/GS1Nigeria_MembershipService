75f105fd33a060a7337a126133973d8611da8089
Samuel Esezobor <prodigy4801@gmail.com>
2022-08-17 16:25:09 +0100

    Commit New Class creation for GCPInformationRepo and PharmaceuticalInformation

---
+++ MembershipPortal.core/Repository/GCPInformationRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MembershipPortal.data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MembershipPortal.core.Repository
+{
+    public class GCPInformationRepo : GenericRepository<GCPInformation>, IGCPInformationRepo
+    {
+        public GCPInformationRepo(ApplicationDBContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ MembershipPortal.core/Repository/PharmaceuticalInformationRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MembershipPortal.data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MembershipPortal.core.Repository
+{
+    public class PharmaceuticalInformationRepo : GenericRepository<PharmaceuticalInformation>, IPharmaceuticalInformationRepo
+    {
+        public PharmaceuticalInformationRepo(ApplicationDBContext context) : base(context)
+        {
+
+        }
+    }
+}
