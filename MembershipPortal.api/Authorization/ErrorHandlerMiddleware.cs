456ebfeb492d5b4ff15e707f34215b9c55a6d6c8
Samuel Esezobor <prodigy4801@gmail.com>
2023-01-19 08:52:56 +0100

    Commit New Class Created for middleware handler.

---
+++ MembershipPortal.api/Authorization/ErrorHandlerMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using MembershipPortal.api.Helpers;
+using MembershipPortal.api.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MembershipPortal.api.Authorization
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await this._next(context);
+            }
+            catch (Exception e)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (e)
+                {
+                    case AppException error:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    case KeyNotFoundException error:
+                        //not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        //unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize<ServiceResponse<object>>(new ServiceResponse<object>{ IsSuccess = false, Message = e?.Message, ReturnedObject = null });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
---
+++ MembershipPortal.api/Authorization/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,37 @@
+﻿using MembershipPortal.api.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MembershipPortal.api.Authorization
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new CustomErrorHandler()
+                        {
+                             message = contextFeature.Error.Message,
+                             statuscode = context.Response.StatusCode
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
